703d4368d9f3dd34a2d4606c9a28bdc1b5c2cd20
suppixie <mamthapatalay6@gmail.com>
2025-08-13 22:09:00 +0100

    refined

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Inbox from './Inbox';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Inbox />
+    <App />
 );
 
 
